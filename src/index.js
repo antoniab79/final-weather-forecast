2ac7f6ecbc349f0daaae7a334b4fdf0bd1aad7fd
antoniab79 <antonia@webleviathan.com>
2024-06-02 22:23:24 +0100

    Finished project - updated index.js and css

--- src/index.js
+++ src/index.js
@@ -59,26 +59,35 @@ function getForecast(city) {
   axios.get(apiUrl).then(displayWeeklyForecast);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function displayWeeklyForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-<div class="weather-forecast-date">${day}</div>
- <div class="weather-forecast-icon">☀</div>
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
   <div class="weather-forecast-temps">
-   <div class="weather-temp"> <strong>18°</strong>
+   <div class="weather-temp"> 
+   <strong>${Math.round(day.temperature.maximum)}°</strong>
    </div>
-   <div class="weather-temp">12°</div>
+   <div class="weather-temp">${Math.round(day.temperature.minimum)}°</div>
    </div> 
 </div>
 `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
